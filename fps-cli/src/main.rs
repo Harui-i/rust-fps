2d15b50f9fc2a39cfa90ad592e0de8d7756b2cc3
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2025-11-10 22:35:43 +0900

    最大次数を引数にとるように

--- fps-cli/src/main.rs
+++ fps-cli/src/main.rs
@@ -2,20 +2,23 @@ use anyhow::Result;
 use clap::Parser;
 use fps_core::{evaluator, parser, tokenizer};
 
-const MAX_DEGREE: usize = 3;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// The expression to parse
     expression: String,
+
+    /// Maximum degree of the resulting power series (default: 5)
+    #[arg(short, long)]
+    maxdeg: Option<usize>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let tokens = tokenizer::tokenize(&args.expression)?;
     let ast = parser::parse(&tokens)?;
-    let series = evaluator::evaluate(&ast, MAX_DEGREE)?;
+    let series = evaluator::evaluate(&ast, args.maxdeg.unwrap_or(5))?;
     println!("{}", series);
     Ok(())
 }
